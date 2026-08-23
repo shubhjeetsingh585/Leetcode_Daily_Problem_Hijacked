cbc4316b6566c011a2611a18abd81e9bf642f131
Shubhjeet Singh <shubhjeetsingh585@gmail.com>
2026-08-23 16:19:05 +0530

    Time: 8 ms (53.28%), Space: 47 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,15 @@
+class Solution{
+    public boolean sumGame(String num){
+        int n=num.length();
+        int leftS=0,rightS=0,leftQ=0,rightQ=0;
+        for(int i=0;i<n/2;i++){
+            if(num.charAt(i)=='?') leftQ++;
+            else leftS+=num.charAt(i)-'0';
+        }
+        for(int i=n/2;i<n;i++){
+            if(num.charAt(i)=='?') rightQ++;
+            else rightS+=num.charAt(i)-'0';
+        }
+        return 2*(leftS-rightS)!=(rightQ-leftQ)*9;
+    }
+}
