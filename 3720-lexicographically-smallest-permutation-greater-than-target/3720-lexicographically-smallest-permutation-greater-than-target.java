df4ac55b0c618a2e8d62344dc41acac634b2d539
Shubhjeet Singh <shubhjeetsingh585@gmail.com>
2026-08-27 13:25:05 +0530

    Time: 2 ms (91.94%), Space: 45 MB (50%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String lexGreaterPermutation(String s, String target){
+        int[] cnt=new int[26];
+        for(char c:s.toCharArray()) cnt[c-'a']++;
+        return dfs(0,true,cnt,target,new StringBuilder());
+    }
+    private String dfs(int i,boolean same,int[] cnt,String t,StringBuilder cur){
+        if(i==t.length()) return same ? "" : cur.toString();
+        int start=same ? t.charAt(i)-'a' : 0;
+        for(int c=start;c<26;c++){
+            if(cnt[c]==0) continue;
+            if(same && c<start) continue;
+            cnt[c]--;
+            cur.append((char)('a'+c));
+            String res=dfs(i+1,same && c==start,cnt,t,cur);
+            if(!res.equals("")) return res;
+            cur.deleteCharAt(cur.length()-1);
+            cnt[c]++;
+        }
+        return "";
+    }
+}
