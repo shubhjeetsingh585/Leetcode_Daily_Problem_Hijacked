2d4011790e47b387a6c5ed1091ff495dc9300d10
Shubhjeet Singh <shubhjeetsingh585@gmail.com>
2026-09-26 02:10:35 +0530

    Time: 8 ms (92.91%), Space: 47.3 MB (47.24%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -0,0 +1,30 @@
+class Solution{
+    int i=0;
+    public List<String> braceExpansionII(String expression){
+        return new ArrayList<>(parse(expression));
+    }
+    private Set<String> parse(String s){
+        Set<String>res=new TreeSet<>();
+        res.add("");
+        while(i<s.length() && s.charAt(i)!='}'){
+            if(s.charAt(i)==','){
+                i++;
+                res.addAll(parse(s));
+                return res;
+            }
+            Set<String>cur=new TreeSet<>();
+            if (s.charAt(i)=='{'){
+                i++;
+                cur=parse(s);
+                i++;
+            } 
+            else cur.add(String.valueOf(s.charAt(i++)));
+            Set<String>next=new TreeSet<>();
+            for(String a:res){
+                for(String b:cur) next.add(a+b);
+            }
+            res=next;
+        }
+        return res;
+    }
+}
